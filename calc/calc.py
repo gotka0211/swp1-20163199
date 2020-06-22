e7447129f2fc0e0e2dfb0ca5650f2619baa9d667
gotka0211 <gotka0211@kookmin.ac.kr>
2020-06-22 09:57:30 +0900

    issue #10 isdigit함수를 활용해 쿼리로 받는 a와b의 값 모두 정수일 때 합과 곱을 정상적으로 출력하도록 만들었습니다.

--- calc/calc.py
+++ calc/calc.py
@@ -5,12 +5,16 @@ def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
 	a = d.get('a',[''])[0]
 	b = d.get('b',[''])[0]
-	sum, mul ='None','None' 
+	sum, mul = 'None','None' 
 	if '' not in [a,b]:
-		sum = int(a)+int(b)
-		mul = int(a)*int(b)
+		if a.isdigit() and b.isdigit():
+			sum = int(a)+int(b)
+			mul = int(a)*int(b)
+		else :
+			sum = 'Error'
+			mul = 'Error'
 	else : a,b = 'None','None'
-	response_body = html % {'a':a,'b':b,'sum':str(sum), 'mul':str(mul)}
+	response_body = html % {'a':a, 'b':b, 'sum':str(sum), 'mul':str(mul)}
 	start_response('200 OK',[
 		('Content-Type', 'text/html'),
 		('Content-Length',str(len(response_body)))])
