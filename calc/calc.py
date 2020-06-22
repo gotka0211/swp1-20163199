9a168bc89a4ca766e84251d07f668b25a1ef3d89
gotka0211 <gotka0211@kookmin.ac.kr>
2020-06-22 09:04:08 +0900

    Issue #9 조건문에 else문을 추가해 입력이 없을시 a와b에 'None'이 출력되도록함. 추가로 a+b와 a*b도 입력이 없을 시 'None'이 출력되도록 함.

--- calc/calc.py
+++ calc/calc.py
@@ -5,12 +5,12 @@ def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
 	a = d.get('a',[''])[0]
 	b = d.get('b',[''])[0]
-	sum, mul = 0, 0
+	sum, mul ='None','None' 
 	if '' not in [a,b]:
-		a,b =int(a),int(b)
-		sum = a+b
-		mul = a*b
-	response_body = html % {'a':a,'b':b,'sum':sum, 'mul':mul}
+		sum = int(a)+int(b)
+		mul = int(a)*int(b)
+	else : a,b = 'None','None'
+	response_body = html % {'a':a,'b':b,'sum':str(sum), 'mul':str(mul)}
 	start_response('200 OK',[
 		('Content-Type', 'text/html'),
 		('Content-Length',str(len(response_body)))])
